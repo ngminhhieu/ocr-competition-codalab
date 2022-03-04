a2e6d54186b6335e5aa7145cf2c1739369393c62
thuydung-icthust <Hung.nn184118@sis.hust.edu.vn>
2022-03-04 16:08:00 +0700

    test

--- package.sh
+++ package.sh
@@ -4,13 +4,17 @@ rm competition.zip
 
 zip -j scoring_program.zip ./scoring_program/*
 
-zip -j public_ground_truth.zip ./public/ground_truth.csv
+zip -j public_ground_truth.zip ./data/public/ground_truth.csv
 
-zip -j private_ground_truth.zip ./private/ground_truth.csv
+zip -j private_ground_truth.zip ./data/private/ground_truth.csv
 
-zip -j warmup_ground_truth.zip ./warm_up/ground_truth.csv
+zip -j warmup_ground_truth.zip ./data/warm_up/ground_truth.csv
 
-zip competition.zip competition.yaml data.html evaluation.html logo.png overview.html terms.html scoring_program.zip public_ground_truth.zip private_ground_truth.zip warmup_ground_truth.zip
+zip -j competition.zip html/award.html html/dataset.html html/evaluation.html html/overview.html html/submission.html html/tasks.html html/terms.html
+
+zip -j competition.zip competition.yaml scoring_program.zip public_ground_truth.zip private_ground_truth.zip warmup_ground_truth.zip
+
+zip -j competition.zip log/logo.svg
 
 rm public_ground_truth.zip
 
