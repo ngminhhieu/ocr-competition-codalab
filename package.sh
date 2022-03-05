341c048f270ad542445d5301c6d2fe8a4a222551
thuydung-icthust <Hung.nn184118@sis.hust.edu.vn>
2022-03-05 21:45:56 +0700

    fix yaml

--- package.sh
+++ package.sh
@@ -4,17 +4,17 @@ rm competition.zip
 
 zip -j scoring_program.zip ./scoring_program/*
 
-zip -j public_ground_truth.zip ./data/public/ground_truth.csv
+zip -j public_ground_truth.zip ./data/public/gt_IC15.zip
 
-zip -j private_ground_truth.zip ./data/private/ground_truth.csv
+zip -j private_ground_truth.zip ./data/private/gt_IC15.zip
 
-zip -j warmup_ground_truth.zip ./data/warm_up/ground_truth.csv
+zip -j warmup_ground_truth.zip ./data/warm_up/gt_IC15.zip
 
-zip -j competition.zip html/award.html html/dataset.html html/evaluation.html html/overview.html html/submission.html html/tasks.html html/terms.html
+zip -j competition.zip html/award.html html/data.html html/evaluation.html html/overview.html html/submission.html html/terms.html
 
 zip -j competition.zip competition.yaml scoring_program.zip public_ground_truth.zip private_ground_truth.zip warmup_ground_truth.zip
 
-zip -j competition.zip log/logo.svg
+zip -j competition.zip logo/logo.svg
 
 rm public_ground_truth.zip
 
