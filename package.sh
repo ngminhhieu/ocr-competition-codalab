7446bffc0e3f7cf51e8beb755eb8272d0dc385da
thuydung-icthust <Hung.nn184118@sis.hust.edu.vn>
2022-03-07 21:49:45 +0700

    fix zip file

--- package.sh
+++ package.sh
@@ -4,9 +4,9 @@ rm competition.zip
 
 zip -j scoring_program.zip ./scoring_program/*
 
-zip -j public_ground_truth.zip ./data/public/gt_IC15.zip
+zip -j public_ground_truth.zip ./data/public/groundtruth.zip
 
-zip -j private_ground_truth.zip ./data/private/gt_IC15.zip
+zip -j private_ground_truth.zip ./data/private/groundtruth.zip
 
 zip -j competition.zip html/award.html html/data.html html/evaluation.html html/overview.html html/submission.html html/terms.html
 
--- scoring_program/evaluate.py
+++ scoring_program/evaluate.py
@@ -4,34 +4,29 @@ import sys
 import rrc_evaluation_funcs
 from script import default_evaluation_params, validate_data, evaluate_method
 import subprocess
+from zipfile import ZipFile
+from os.path import basename
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 install("Polygon2")
+
+def zip_files(name, folder):
+    if os.path.exists("{}/{}.zip".format(folder, name)):
+        os.remove("{}/{}.zip".format(folder, name))
+    with ZipFile('{}/{}.zip'.format(folder, name), 'w') as zipObj:
+        for folderName, subfolders, filenames in os.walk(folder):
+            for filename in filenames:
+                if filename.endswith(".txt"):
+                    filePath = os.path.join(folderName, filename)
+                    zipObj.write(filePath, basename(filePath))
+    os.system("rm {}/*.txt".format(folder))
 
 if __name__ == "__main__":
     [_, input_dir, output_dir] = sys.argv
     submission_dir = os.path.join(input_dir, 'res')
     truth_dir = os.path.join(input_dir, 'ref')
-
-    # ground_truth_path = os.path.join(truth_dir, 'ground_truth.txt')
-    # submission_path = os.path.join(submission_dir, 'results.txt')
-
-    # ground_truth = pd.read_csv(ground_truth_path, sep=',', header=None, names=['x1','y1','x2','y2','x3','y3', 'x4', 'y4', 'label'])
-    # submission = pd.read_csv(submission_path, sep=',', header=None, names=['x1','y1','x2','y2','x3','y3', 'x4', 'y4', 'label'])
-    # f1 = None
-    # res_dict = rrc_evaluation_funcs.main_evaluation({'g': '{}/gt_IC15.zip'.format(truth_dir), 's': '{}/gt_IC15.zip'.format(submission_dir)}, default_evaluation_params, validate_data, evaluate_method)
-    
-    # def list_files(startpath):
-    #     for root, dirs, files in os.walk(startpath):
-    #         level = root.replace(startpath, '').count(os.sep)
-    #         indent = ' ' * 4 * (level)
-    #         print('{}{}/'.format(indent, os.path.basename(root)))
-    #         subindent = ' ' * 4 * (level + 1)
-    #         for f in files:
-    #             print('{}{}'.format(subindent, f))
-    # list_files("../program")
-    res_dict = rrc_evaluation_funcs.main_evaluation({'g': '{}/gt_IC15.zip'.format(truth_dir), 's': '{}/gt_IC15.zip'.format(submission_dir)}, default_evaluation_params, validate_data, evaluate_method)
-    print(res_dict['method'])
+    zip_files('prediction', submission_dir)
+    res_dict = rrc_evaluation_funcs.main_evaluation({'g': '{}/groundtruth.zip'.format(truth_dir), 's': '{}/prediction.zip'.format(submission_dir)}, default_evaluation_params, validate_data, evaluate_method)
     with open(os.path.join(output_dir, 'scores.txt'), 'w') as output_file:
-        output_file.write("CER: {:f}\n".format(round(res_dict['method']['cer'],6)))
-        output_file.write("F1: {:f}".format(round(res_dict['method']['hmean'],6)))
+        output_file.write("CER: {:f}\n".format(round(res_dict['method']['cer'], 4)))
+        output_file.write("F1: {:f}".format(round(res_dict['method']['hmean'], 4)))
